84498519bbef50758642d4ff07c81512e96bde35
Levan <sunshine603@internet.ru>
2024-12-01 22:00:38 +0300

    del layout

---
+++ app/about/me/layout.tsx
@@ -0,0 +1,15 @@
+export default function AboutLayout({
+    children,
+}: { 
+
+    children: React.ReactNode
+}) {
+    return ( 
+        <div>
+            <div className="h-24 w-full bg-blue-900">
+                <h1 className="text-white text-2x1">About</h1>
+            </div>
+            {children}
+        </div>
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,9 +26,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <nav>
+          <ul className="flex gap-2 justify-between px-4 bg-blue-700">
+            <li>
+              <Link href={'/'}>Home</Link>
+            </li>
+            <li>
+              <Link href={'/about'}>About</Link>
+            </li>
+          </ul>
+        </nav>
         {children}
       </body>
     </html>
