33222b095301fe4822dc6cc8ba5f59420e8ab78a
Levan <sunshine603@internet.ru>
2024-12-08 20:29:31 +0300

    middleware

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 
 export default function Home() {
-  throw new Error("An error occurred")
   return (
       <main className="">
         <h1>Welcome to next.js</h1>
